8c7b155674c5c6fc80539eb35fa2b6fa4a681b8f
vaishnavi <vaishu.srini15@gmail.com>
2021-06-09 07:56:11 -0500

    Added script
    Signed-off-by: vaishnavi <vaishu.srini15@gmail.com>

--- script.js
+++ script.js
@@ -1,1 +1,8 @@
 // TODO: add code here
+window.addEventListener("load",function(){
+    fetch("https://handlers.education.launchcode.org/static/astronauts.json").then(function(response){
+        response.json().then(function(json){
+            console.log(json);
+        });
+    });
+});
